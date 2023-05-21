8b931662e096d107e978f0d79b8baf69f7dff426
Abhishek Soni <asoni1@binghamton.edu>
2023-05-21 00:39:12 -0400

    Time: 142 ms (21.01%), Space: 45 MB (5.31%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,82 +1,64 @@
 class Solution {
-    public String minWindow(String searchString, String t) {
-      
-    // Creating Map for storing the frequency
-    Map<Character, Integer> requiredCharacters = buildMappingOfCharactersToOccurrences(t);
-    Map<Character, Integer> windowCharacterMapping = new HashMap<Character, Integer>();
-
-    int left = 0;
-    int right = 0;
-
-    int totalCharFrequenciesToMatch = requiredCharacters.size();
-    int charFrequenciesInWindowThatMatch = 0;
-
-    // This will store the minimum length of valid substring
-    int minWindowLengthSeenSoFar = Integer.MAX_VALUE;
-
-    // It will store the actual substring
-    String minWindow = "";
-
-    // Here we calculate the ans using 2 pointer's approach
-    while (right < searchString.length()) {
-      char characterAtRightPointer = searchString.charAt(right);
-      addCharacterToHashtableMapping(windowCharacterMapping, characterAtRightPointer);
-
-      boolean rightCharIsARequirement = requiredCharacters.containsKey(characterAtRightPointer);
-      if (rightCharIsARequirement) {
-        boolean requirementForCharacterMet = requiredCharacters.get(characterAtRightPointer)
-            .intValue() == windowCharacterMapping.get(characterAtRightPointer).intValue();
-
-        if (requirementForCharacterMet) {
-          charFrequenciesInWindowThatMatch++;
+    public String minWindow(String s, String t) {
+        
+        Map<Character, Integer> sMap = new HashMap();
+        
+        Map<Character, Integer> tMap = new HashMap();
+        
+        int have = 0 ;
+        int need = 0;
+        
+        
+        for ( int i = 0 ; i < t.length(); i++ ){
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
         }
-      }
+        
+        need = tMap.size();
+        
+        int start = 0;
+        int end = 0;
+        int minLength = Integer.MAX_VALUE;
+        
+        String minWindow = "";
+        
+        while ( end < s.length() ){
+            
+            char c = s.charAt(end);
+            
+            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+            
+            
+            if ( tMap.containsKey(c) ){
+                
+                if ( sMap.get(c).intValue() == tMap.get(c).intValue() ){
+                    have++;
+                }
+                
+            }
+            
+            while ( have == need && start <= end ) {
+                if ( minLength > (end-start+1) ){
+                    minLength = (end-start+1);
+                    minWindow = s.substring(start, end + 1);
+                }
+                
+                sMap.put( s.charAt(start), sMap.get(s.charAt(start))-1);
 
-      while (charFrequenciesInWindowThatMatch == totalCharFrequenciesToMatch && left <= right) {
-        char characterAtLeftPointer = searchString.charAt(left);
-        int windowSize = right - left + 1;
-
-        if (windowSize < minWindowLengthSeenSoFar) {
-          minWindowLengthSeenSoFar = windowSize;
-          minWindow = searchString.substring(left, right + 1);
+                if ( tMap.containsKey( s.charAt(start) ) && sMap.get(s.charAt(start)).intValue() < tMap.get( s.charAt(start) ).intValue() ){
+                    have--;
+                }
+                
+                start++;
+                
+            }
+            
+            end++;
+            
         }
-
-        windowCharacterMapping.put(characterAtLeftPointer, windowCharacterMapping.get(characterAtLeftPointer) - 1);
-
-        boolean leftCharIsARequirement = requiredCharacters.containsKey(characterAtLeftPointer);
-        if (leftCharIsARequirement) {
-          boolean characterFailsRequirement = windowCharacterMapping.get(characterAtLeftPointer)
-              .intValue() < requiredCharacters.get(characterAtLeftPointer).intValue();
-
-          if (characterFailsRequirement) {
-            charFrequenciesInWindowThatMatch--;
-          }
-        }
-
-        left++;
-      }
-
-      right++;
+        
+        
+        
+        return minWindow;
+        
     }
-
-    return minWindow;
-  }
-
-  // Helper function for computing the character's frequency of a string
-  private Map<Character, Integer> buildMappingOfCharactersToOccurrences(String s) {
-    Map<Character, Integer> map = new HashMap<Character, Integer>();
-
-    for (int i = 0; i < s.length(); i++) {
-      int occurrencesOfCharacter = map.getOrDefault(s.charAt(i), 0);
-      map.put(s.charAt(i), occurrencesOfCharacter + 1);
-    }
-
-    return map;
-  }
-  
-  // Helper function to insert a character in the map
-  private void addCharacterToHashtableMapping(Map<Character, Integer> map, Character c) {
-    int occurrences = map.getOrDefault(c, 0);
-    map.put(c, occurrences + 1);
-  }
 }
