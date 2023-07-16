414e2b51afc8fa7291ccc81e8a74fb93d623791b
Abhishek Soni <asoni1@binghamton.edu>
2023-07-16 11:05:00 -0400

    Time: 1 ms (95.91%), Space: 44.8 MB (15.46%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,43 +1,49 @@
 class Solution {
     public int trap(int[] height) {
         
-        int lMax = 0;
-        int rMax = 0;
-        
-        int waterTrap = 0;
-        
         int start = 0;
         int end = height.length - 1;
         
+        int lMax = height[start];
+        int rMax = height[end];
+        
+        int waterTrap = 0;
+    
+        
         while(start < end){
-            
-            // int temp = lMax < rMax ? lMax - height[start] : rMax - height[end];
             
             int temp = 0;
             
             if( lMax <= rMax ){
-                temp = lMax - height[start];
-            }else{
-                temp = rMax - height[end];
-            }
-            
-            if(temp > 0){
-                waterTrap += temp;
-            }
-            
-            if ( lMax < height[start]){
-                lMax = height[start];
-            }
-            
-            if( rMax < height[end]){
-                rMax = height[end];
-            }
-            
-            if( lMax <= rMax ){
                 start++;
+                
+                lMax = lMax < height[start] ? height[start] : lMax;
+                
+                waterTrap += lMax - height[start];
             }else{
                 end--;
+                
+                rMax = rMax < height[end] ? height[end] : rMax;
+                
+                waterTrap += rMax - height[end];
             }
+            
+//             if(temp > 0){
+//                 waterTrap += temp;
+//             }
+            
+//             if ( lMax < height[start]){
+//                 lMax = height[start];
+//             }
+            
+//             if( rMax < height[end]){
+//                 rMax = height[end];
+//             }
+            
+//             if( lMax <= rMax ){
+//             }else{
+//                 end--;
+//             }
             
         }
         
